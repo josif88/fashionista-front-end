b1e0e55fd4482cb0d03842d1da572f478c7471ab
Yousif Mubareka <yousifmubarka@gmail.com>
2020-11-15 12:53:54 +0300

    improvments

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,10 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Items from "../components/items";
 
-
 //make it work offline
 // ServiceWorker.register();
 
 export default function Home(props) {
-
   return (
     <div>
       <Header pageTitle={"Whats New"} />
@@ -17,10 +15,9 @@ export default function Home(props) {
   );
 }
 
-
 //get latest items data and pass it to item list components
 export async function getServerSideProps() {
-  const resp = await fetch(`https://fashionista.fans/api/items?limit=25`);
+  const resp = await fetch(`https://fashionista.fans/api/items?limit=5`);
   const items = await resp.json();
   return {
     props: {
--- pages/new.js
+++ pages/new.js
@@ -2,10 +2,7 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import Items from "../components/items";
 
-
-
 export default function Home(props) {
-
   return (
     <div>
       <Header pageTitle={"Whats New"} />
@@ -15,10 +12,9 @@ export default function Home(props) {
   );
 }
 
-
 //get latest items data and pass it to item list components
 export async function getServerSideProps() {
-  const resp = await fetch(`https://fashionista.fans/api/items?limit=25`);
+  const resp = await fetch(`https://fashionista.fans/api/items?limit=5`);
   const items = await resp.json();
   return {
     props: {
@@ -26,4 +22,3 @@ export async function getServerSideProps() {
     },
   };
 }
-
